c9e668c853025052f44efabf6c18df56de3505ce
madara-tribe <tsukuba.univer@gmail.com>
2022-08-24 08:59:06 +0900

    update

--- backup.sh
+++
@@ -1,8 +0,0 @@
-# /bin/sh
-mkdir bp
-mkdir bp/Docker bp/GCNet bp/networks bp/utils
-cp *.py *.sh bp/
-cp Docker/* bp/Docker/
-cp GCNet/*.py bp/GCNet/
-cp networks/*.py bp/networks/
-cp utils/*.py bp/utils/
--- reset.sh
+++
@@ -1,4 +0,0 @@
-#/bin/sh
-rm -r logs
-find . | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
-find . -name '.DS_Store' -type f -ls -delete
